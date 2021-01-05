5d3e00623a85a6044c92dd514f05adaa1aec1790
Yigit Kemal Erinc <yigit.erinc@ozu.edu.tr>
2021-01-06 01:07:25 +0300

    Init project, work on toolbar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import router from './router'
+
+import { MdButton, MdToolbar } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdToolbar);
+Vue.use(MdButton);
+
+Vue.use(VueRouter);
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <md-toolbar class="md-transparent">
+      <h3 class="md-title">Yiğit Medikal</h3>
+      <div class="md-toolbar-section-end">
+        <p @click="redirectTo('urunler')">Ürünler</p>
+        <p>Hakkımızda</p>
+        <p>İletişim</p>
+      </div>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    redirectTo: function (endpoint) {
+      return this.$router.push(endpoint);
+    }
+  }
+
+}
+</script>
+
+<style>
+
+  .home .md-toolbar-section-end a, p {
+    margin-right: 2rem;
+    font-size: 17px;
+    text-decoration: none;
+  }
+
+</style>
