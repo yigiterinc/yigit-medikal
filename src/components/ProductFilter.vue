84aa90bbab165a860f429dfee6eaf583f3a946d4
Yigit Kemal Erinc <yigit.erinc@ozu.edu.tr>
2021-02-14 00:54:16 +0300

    Implement filter components, enhance grid UI

---
+++ src/components/ProductFilter.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="product-filter">
+      <div v-for="(filter, filterIndex) in filters"
+           :key="filterIndex">
+          <h5 class="label">{{ filter.label }}</h5>
+          <div class="ml-3">
+            <b-form-checkbox
+                v-for="(option, optionIndex) in filter.options"
+                v-model="filter.selected[optionIndex]"
+                :value="option"
+                :key="optionIndex"
+                @change="optionSelected(filterIndex, optionIndex)"
+            >
+              {{ option }}
+            </b-form-checkbox>
+          </div>
+        <hr class="separator"/>
+      </div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "ProductFilter",
+    props: {
+      filters: {
+        type: Array,
+        required: true
+      }
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+      optionSelected(filterIndex, optionIndex) {
+        this.$emit('optionSelected', {
+          filterIndex: filterIndex,
+          optionIndex: optionIndex,
+          value: this.filters[filterIndex].selected[optionIndex]
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .product-filter {
+    width: auto;
+    background: #f9fbfc;
+    box-shadow: #c5d9db 0 0 10px;
+    border-radius: 5px;
+    padding-top: 10px;
+  }
+
+  .separator {
+    width: 100%;
+  }
+
+  .label {
+    margin-left: 10px;
+
+  }
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="products" v-if="this.products">
-    <b-container class="pt-4">
-      <b-row v-for="(productGroup, index) in groupedPaginatedProducts"
-             class="mb-4"
-             :key="index">
-        <b-col v-for="product in productGroup" :key="product.id">
-          <div>
-            <img class="product-image" :src="product.photo_path" alt="">
-            <br>
-            {{product.name}}
-          </div>
+  <div class="products" v-if="this.product.products">
+    <b-container class="products-main-container pt-4">
+      <b-row>
+        <b-col v-if="productCategoryIsNotEmpty" class="col-sm-3">
+          <product-filter :filters="filters"
+                          @optionSelected="onOptionSelected">
+          </product-filter>
+        </b-col>
+        <b-col>
+        <b-row v-for="(productGroup, index) in groupedPaginatedProducts"
+               class="mb-4"
+               :key="index">
+          <b-col v-for="product in productGroup" :key="product.id">
+            <div>
+              <img class="product-image" :src="product.photo_path" alt="">
+              <br>
+              {{product.name}}
+            </div>
+          </b-col>
+        </b-row>
+
         </b-col>
       </b-row>
 
       <b-row class="justify-content-center">
         <b-pagination
-            v-model="currentPage"
+            v-model="pagination.currentPage"
             :total-rows="rows"
-            :per-page="perPage"
+            :per-page="pagination.perPage"
             @change="onPageChanged"
             pills
             class="pagination-style mt-4"
@@ -30,75 +40,142 @@
 
 <script>
   import Products from "@/data/products"
+  import { TYPES } from "@/data/types.js";
+
+  import ProductFilter from "@/components/ProductFilter"
+
   import _ from 'lodash';
 
   export default {
     name: "Products",
+    components: {
+      ProductFilter
+    },
     data() {
       return {
-        products: null,
-        filteredProducts: null,
-        paginatedProducts: this.products,
-        perPage: 9,
-        currentPage: 1,
-        productType: ''
+        product: {
+          products: null,
+          productCategory: '',
+          productsFilteredByCategory: null,
+          productsFilteredByBrandAndAttribute: [],
+          paginatedProducts: this.products,
+        },
+        pagination: {
+          perPage: 9,
+          currentPage: 1,
+        },
+        filters: [],
+        brandsSelected: {},
+        attributesSelected: {},
+        BRANDS_FILTER_INDEX: 0,
+        ATTRIBUTES_FILTER_INDEX: 1
       }
     },
     computed: {
       rows() {
-        return this.products.length
+        return this.product.products.length;
       },
       groupedPaginatedProducts() {
-        return _.chunk(this.paginatedProducts, 3);
+        return _.chunk(this.product.paginatedProducts, 3);
+      },
+      productCategoryIsNotEmpty() {
+        return this.product.productCategory !== undefined && this.product.productCategory.length > 0;
       }
     },
     watch: {
       '$route.params.type': {
         handler: function (val) {
-          if (this.productType) {
+          if (this.product.productCategory) {
             console.log(val);
-            this.productType = val;
-            this.filteredProducts = this.products.filter(product => product.type.name === val)
+            this.product.productCategory = val;
+            this.product.productsFilteredByCategory = this.product.products.filter(product => product.type.name === val)
           }
         },
         deep: true,
         immediate: true
       },
       productType: function () {
-        if (this.filteredProducts) {
-          this.paginate(this.perPage, 0);
+        if (this.product.productsFilteredByCategory) {
+          this.paginate(this.pagination.perPage, 0);
         }
+      },
+      brandsSelected: {
+        handler: function () {
+            console.log('for some random reason, i am broke, fix me please')  // TODO
+        },
+        deep: true
       }
     },
     methods: {
       paginate(page_size, page_number) {
-        let itemsToParse = this.filteredProducts;
-        this.paginatedProducts = itemsToParse.slice(page_number * page_size, (page_number + 1) * page_size
-        );
+          let itemsToParse = this.product.productsFilteredByCategory;
+          this.product.paginatedProducts = itemsToParse.slice(page_number * page_size, (page_number + 1) * page_size);
       },
       onPageChanged(page) {
-        this.paginate(this.perPage, page - 1);
+        this.paginate(this.pagination.perPage, page - 1);
+      },
+      onOptionSelected({filterIndex, optionIndex, value}) {
+        if (filterIndex === this.BRANDS_FILTER_INDEX) {
+          this.brandsSelected[optionIndex] = value;
+          console.log(this.brandsSelected)
+        } else if (filterIndex === this.ATTRIBUTES_FILTER_INDEX) {
+          this.attributesSelected[optionIndex] = value
+        }
+      },
+      fillFilters(types, productType) {
+        for (const key in types) {
+          if (Object.prototype.hasOwnProperty.call(types, key)) {
+            const type = types[key]
+            if (type.name === productType) {
+              const brands = Object.values(type.brands);
+              const attributes = Object.values(type.attributes);
+
+              this.filters = [
+                {
+                  label: "Markalar",
+                  options: brands,
+                  selected: Array(brands.length).fill(false)
+                },
+                {
+                  label: "Özellikler",
+                  options: attributes,
+                  selected: Array(attributes.length).fill(false)
+                }
+              ]
+
+              return;
+            }
+          }
+        }
       }
     },
     mounted() {
-      this.paginate(this.perPage, 0);
+      this.paginate(this.pagination.perPage, 0);
     },
     created() {
-      this.products = Products.PRODUCTS;
-      this.productType = this.$route.params.type;
-      this.filteredProducts = this.products;
+      this.product.products = Products.PRODUCTS;
+      this.product.productCategory = this.$route.params.type;
+      this.product.productsFilteredByCategory = this.products;
 
-      if (this.productType) {
-        this.filteredProducts = this.products.filter(product => product.type.name === this.productType)
+      if (this.product.productCategory) {
+        this.product.productsFilteredByCategory = this.product.products
+            .filter(product => product.type.name === this.product.productCategory)
+        this.fillFilters(TYPES, this.product.productCategory);
       }
     }
   }
 </script>
 
 <style scoped>
+
   .products {
     height: 100vh;
     position: relative;
+  }
+
+  .products-main-container {
+    padding-right: 0;
+    margin-right: 25vw;
   }
 
   .product-image {
@@ -112,4 +189,5 @@
     margin-top: 60px;
     margin-bottom: 50px;
   }
+
 </style>
